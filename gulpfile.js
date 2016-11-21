ec768ef00e7a96ca551aa461c9388028344693ab
my-helper <get.it@zoho.com>
2016-11-21 17:52:12 +0300

    go to gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp'),
   plugins = require('gulp-load-plugins')(),
   webpack = require('webpack-stream'),
+  del = require('del'),
   browserSync = require('browser-sync').create();
 
 const paths = {
@@ -20,38 +21,40 @@ function getTask(task) {
   return require('./gulp-tasks/' + task)(gulp, plugins, paths);
 }
 
+const clean = () => del([ 'build', '.tmp', '.publish' ]);
+
 // Get one .less file and render
 gulp.task('html', getTask('html'));
 gulp.task('css', getTask('css'));
 gulp.task('js', getTask('js'));
 
 gulp.task('css-min', getTask('css-min'));
-gulp.task('html-min', ['css-min'], getTask('html-min'));
+gulp.task('html-min', getTask('html-min'));
 gulp.task('js-min', getTask('js-min'));
-
-gulp.task('copy', ['copy-fonts'], () =>
-  gulp.src(paths.img)
-    .pipe(gulp.dest(paths.dev + 'img'))
-);
 
 gulp.task('copy-fonts', () =>
   gulp.src(paths.fonts)
     .pipe(gulp.dest(paths.dev + 'fonts'))
 );
 
-gulp.task('copy-to-build', ['copy-fonts-to-build'], () =>
+gulp.task('copy', gulp.series('copy-fonts', () =>
   gulp.src(paths.img)
-    .pipe(gulp.dest(paths.build + 'img'))
-);
+    .pipe(gulp.dest(paths.dev + 'img'))
+));
 
 gulp.task('copy-fonts-to-build', () =>
   gulp.src(paths.fonts)
     .pipe(gulp.dest(paths.build + 'fonts'))
 );
 
-gulp.task('css-watch', ['css'], reload());
-gulp.task('html-watch', ['html'], reload());
-gulp.task('js-watch', ['js'], reload());
+gulp.task('copy-to-build', gulp.series('copy-fonts-to-build', () =>
+  gulp.src(paths.img)
+    .pipe(gulp.dest(paths.build + 'img'))
+));
+
+gulp.task('css-watch', gulp.series('css', reload()));
+gulp.task('html-watch', gulp.series('html', reload()));
+gulp.task('js-watch', gulp.series('js', reload()));
 
 function reload() {
   return function(done) {
@@ -59,6 +62,16 @@ function reload() {
     done();
   }
 }
+
+function watch() {
+  gulp.watch(paths.stylusWatch, gulp.series('css-watch'));
+  gulp.watch(paths.pugWatch, gulp.series('html-watch'));
+  gulp.watch(paths.jsWatch, gulp.series('js-watch'));
+  gulp.watch(paths.img, gulp.series('copy'));
+}
+
+exports.watch = watch;
+exports.clean = clean;
 
 // Static server
 gulp.task('serve', function() {
@@ -69,11 +82,6 @@ gulp.task('serve', function() {
     },
     browser: ['chrome', 'google chrome']
   });
-
-  gulp.watch(paths.stylusWatch, ['css-watch']);
-  gulp.watch(paths.pugWatch, ['html-watch']);
-  gulp.watch(paths.jsWatch, ['js-watch']);
-  gulp.watch(paths.img, ['copy']);
 });
 
 const spriteConfig = {
@@ -98,12 +106,17 @@ gulp.task('webpack', () =>
     .pipe(gulp.dest(paths.build + 'js'))
 );
 
-gulp.task('deploy', ['build'], () =>
+const build = gulp.series('html-min', 'css-min', 'js-min', 'copy-to-build');
+
+gulp.task('deploy', gulp.series(build, () =>
   gulp.src(paths.build + '**/*')
     .pipe(plugins.ghPages())
-);
+));
+
+const dev = gulp.parallel('html', 'css', 'js', 'copy', 'serve', watch);
+
+gulp.task('build', build);
+gulp.task('dev', dev);
 
 // The default task (called when you run `gulp` from cli)
-gulp.task('build', ['html-min', 'css-min', 'js-min', 'copy-to-build']);
-gulp.task('dev', ['html', 'css', 'js', 'copy', 'serve']);
-gulp.task('default', ['dev']);
+gulp.task('default', dev);
