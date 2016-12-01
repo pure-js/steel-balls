08a32f863ac00ce09960682d4b703457aa57b406
my-helper <get.it@zoho.com>
2016-12-01 20:13:59 +0300

    some changes

--- src/js/fixed-nav.js
+++ src/js/fixed-nav.js
@@ -1,6 +1,8 @@
 function fixedNav({
   linkEl = document.querySelectorAll('.nav__item'),
-  offset = 39
+  offset = 39,
+  navEl = document.querySelector('.header'),
+  bodyEl = document.querySelector('.under-fixed')
 } = {}) {
 
   const linkLength = linkEl.length;
@@ -11,5 +13,16 @@ function fixedNav({
       anchorClick(this, offset);
       toggleEl(navContainer, 'hidden-sm-down');
     })
+  }
+
+  // console.log(navEl.offsetHeight);
+  function addMarginTop(addTo, getFrom) {
+    addTo.style.marginTop = getFrom.offsetHeight + 'px';
+  }
+
+  addMarginTop(bodyEl, navEl);
+
+  window.onresize = function() {
+    addMarginTop(bodyEl, navEl);
   }
 }
